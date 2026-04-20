5bfe56f6f9922e4e68c345cf964dac3e0eed3d4b
Rikin Marfatia <rikin@supergooey.dev>
2026-04-20 10:10:53 -0700

    Moving the circle

--- app/src/main/java/dev/supergooey/liquidklass/MainActivity.kt
+++ app/src/main/java/dev/supergooey/liquidklass/MainActivity.kt
@@ -1,5 +1,8 @@
 package dev.supergooey.liquidklass
 
+import android.graphics.ColorFilter
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.graphics.RenderEffect
 import android.graphics.RuntimeShader
 import android.graphics.Shader
@@ -7,6 +10,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.EaseInOutQuad
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.animateFloat
@@ -14,48 +18,41 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.CompositingStrategy
 import androidx.compose.ui.graphics.ShaderBrush
 import androidx.compose.ui.graphics.asComposeRenderEffect
-import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.layer.drawLayer
 import androidx.compose.ui.graphics.rememberGraphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.boundsInRoot
 import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import dev.supergooey.liquidklass.shaders.GooeySDF
 import dev.supergooey.liquidklass.shaders.gooeySdf
 import dev.supergooey.liquidklass.ui.theme.LiquidKlassTheme
-import org.intellij.lang.annotations.Language
 
 //@Language("AGSL")
 //val GlassShader = """
@@ -141,14 +138,7 @@ fun App() {
     val backgroundLayer = rememberGraphicsLayer()
     val blurLayer = rememberGraphicsLayer()
     val infinite = rememberInfiniteTransition()
-    val circleOffset by infinite.animateFloat(
-        initialValue = 0f,
-        targetValue = 96f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(easing = LinearEasing, durationMillis = 3000),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
+    var circleOffset by remember { mutableStateOf(Offset.Zero) }
     var circleCenter by remember { mutableStateOf(Offset.Zero) }
     val circleRadius = remember { 40.dp }
     var rectCenter by remember { mutableStateOf(Offset.Zero) }
@@ -209,9 +199,8 @@ fun App() {
 
                     shader.setFloatUniform(
                         "smoothK",
-                        60f
+                        80f
                     )
-
 
                     onDrawWithContent {
                         blurLayer.record { drawLayer(backgroundLayer) }
@@ -237,11 +226,18 @@ fun App() {
             Box(
                 modifier = Modifier
                     .graphicsLayer {
-                        translationX = -(circleOffset).dp.toPx()
+                        translationX = circleOffset.x
+                        translationY = circleOffset.y
                     }
                     .size(80.dp)
                     .onGloballyPositioned { coords ->
                         circleCenter = coords.boundsInRoot().center
+                    }
+                    .pointerInput(Unit) {
+                        detectDragGestures { change, dragAmount ->
+                            change.consume()
+                            circleOffset += dragAmount
+                        }
                     }
             )
         }
