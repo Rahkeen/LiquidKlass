b10a6b0719eb9ea22de94d35defb8e26dd7c617a
Rikin Marfatia <rikin@supergooey.dev>
2026-07-24 21:03:08 -0700

    Building a slider sample

--- app/src/main/java/dev/supergooey/liquidklass/components/LiquidSlider.kt
+++ app/src/main/java/dev/supergooey/liquidklass/components/LiquidSlider.kt
@@ -2,122 +2,109 @@ package dev.supergooey.liquidklass.components
 
 import android.graphics.RenderEffect
 import android.graphics.RuntimeShader
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.awaitEachGesture
-import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.horizontalDrag
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.center
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.asComposeRenderEffect
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.key.Key.Companion.D
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import dev.supergooey.liquidklass.shaders.squircleDisplacementShader
-import dev.supergooey.liquidklass.ui.theme.Blue400
+import dev.supergooey.liquidklass.ui.theme.Green400
 import dev.supergooey.liquidklass.ui.theme.LiquidKlassTheme
 
 @Composable
 fun LiquidSlider() {
     val shader = remember { RuntimeShader(squircleDisplacementShader) }
-    val knobSize = remember { DpSize(width = 40.dp, height = 40.dp) }
-    var knobOffset by remember { mutableStateOf(Offset.Zero) }
+    var knobCenter by remember { mutableStateOf(Offset.Unspecified) }
+    val knobSize by remember { mutableStateOf(20.dp) }
 
-    Canvas(
+    Box(
         modifier = Modifier
             .graphicsLayer {
-                val progress = knobOffset.x.coerceIn(0f, size.width)
+                val center = if (knobCenter == Offset.Unspecified) {
+                    size.center
+                } else {
+                    knobCenter
+                }
                 with(shader) {
                     setFloatUniform(
                         "center",
-                        progress,
-                        size.height / 2f
+                        center.x,
+                        center.y,
                     )
                     setFloatUniform(
                         "halfSize",
-                        knobSize.width.toPx()/2,
-                        knobSize.height.toPx()/2
+                        knobSize.toPx(),
+                        knobSize.toPx(),
                     )
                     setFloatUniform(
                         "squircleN",
-                        2f
+                        2f,
                     )
                     setFloatUniform(
                         "strength",
-                        80f
+                        60f
                     )
                     setFloatUniform(
                         "rampPower",
-                        4f
+                        2f
                     )
+
+                    renderEffect = RenderEffect.createRuntimeShaderEffect(
+                        shader,
+                        "background"
+                    ).asComposeRenderEffect()
                 }
-                renderEffect = RenderEffect.createRuntimeShaderEffect(
-                    shader,
-                    "background"
-                ).asComposeRenderEffect()
+            }
+            .fillMaxSize()
+            .background(color = Color.White)
+            .drawBehind {
+                val padding = 16.dp.toPx()
+                drawLine(
+                    color = Color.LightGray,
+                    start = Offset(padding, size.center.y),
+                    end = Offset(size.width - padding, size.center.y),
+                    strokeWidth = 8.dp.toPx(),
+                    cap = StrokeCap.Round
+                )
+                drawLine(
+                    color = Green400,
+                    start = Offset(padding, size.center.y),
+                    end = Offset((size.width - padding) * 0.5f, size.center.y),
+                    strokeWidth = 8.dp.toPx(),
+                    cap = StrokeCap.Round
+                )
             }
             .pointerInput(Unit) {
-                awaitEachGesture {
-                    val down = awaitFirstDown()
-                    knobOffset = down.position
-                    horizontalDrag(down.id) { change ->
-                        knobOffset = change.position
-                        change.consume()
-                    }
-                }
-            }
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .background(color = Color.White)
-    ) {
-        val progress = Math.clamp(knobOffset.x, 0f, size.width)
-        drawLine(
-            color = Color.LightGray,
-            start = Offset(x = 0f, y = size.center.y),
-            end = Offset(x = size.width, y = size.center.y),
-            strokeWidth = 12f,
-        )
-        drawLine(
-            color = Blue400,
-            start = Offset(x = 0f, y = size.center.y),
-            end = Offset(x = progress, y = size.center.y),
-            strokeWidth = 12f,
-        )
-//        drawCircle(color = Blue400, radius = 30f, center = Offset(progress, size.center.y))
-    }
+                val fallback = Offset(size.width * 0.5f, size.height * 0.5f)
+                detectDragGestures { change, dragAmount ->
+                    change.consume()
+                    val base = if (knobCenter == Offset.Unspecified) fallback else knobCenter
+                    knobCenter = base + dragAmount
+                }            }
+
+    )
 }
 
 @Preview
 @Composable
 private fun LiquidSliderPreview() {
     LiquidKlassTheme {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.White)
-                .padding(32.dp)
-            ,
-            contentAlignment = Alignment.Center
-        ) {
-            LiquidSlider()
-        }
+        LiquidSlider()
     }
 }
