c36a41b3497f2302b63c331e2bba4af287f938bb
Rikin Marfatia <rikin@supergooey.dev>
2026-07-26 22:03:46 -0700

    Fix linebreak

--- app/src/main/java/dev/supergooey/liquidklass/shaders/BevelGlass.kt
+++ app/src/main/java/dev/supergooey/liquidklass/shaders/BevelGlass.kt
@@ -208,8 +208,7 @@ data class BevelShaderConfig(
     val refractionStrength: Float = 40f,
     val fresnelExponent: Float = 2f,
     val rimIntensity: Float = 0.8f,
-    val aberration: Float = 8
-            f,
+    val aberration: Float = 8f,
     val shadowOpacity: Float = 0.35f,
 )
 
